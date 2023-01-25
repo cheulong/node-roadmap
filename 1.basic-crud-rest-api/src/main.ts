e7258f672c606a394f49bc4b8e93dc95fd813348
cheulong <searcheulong@gmail.com>
2023-01-26 02:21:09 +0700

    add end point

--- 1.basic-crud-rest-api/src/main.ts
+++ 1.basic-crud-rest-api/src/main.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hi');
+});
+
+app.get('/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.send(id);
+});
+
+app.post('/', (req: Request, res: Response) => {
+  const { id } = req.body;
+  res.send(id);
+});
+
+app.put('/', (req: Request, res: Response) => {
+  const { id } = req.body;
+  res.send(id);
 });
 
 app.listen(3000);
