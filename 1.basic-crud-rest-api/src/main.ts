ff1e54394e0498961f819b02535f93df3505ab53
cheulong <searcheulong@gmail.com>
2023-01-26 02:12:49 +0700

    setup eslint

---
+++ 1.basic-crud-rest-api/src/main.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hi');
+});
+
+app.listen(3000);
+console.log('hi');
